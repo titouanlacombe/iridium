71c3c2ffa8d85733f3d4a2bb38cd00c866c09d28
Titouan Lacombe <titouan.lacombe99@gmail.com>
2022-11-08 15:50:23 +0100

    optimized child

--- src/quadtree.rs
+++ src/quadtree.rs
@@ -5,8 +5,5 @@ pub struct QuadTree {
     pub width: f32,
     pub height: f32,
 
-    pub child_0: Option<Box<QuadTree>>,
-    pub child_1: Option<Box<QuadTree>>,
-    pub child_2: Option<Box<QuadTree>>,
-    pub child_3: Option<Box<QuadTree>>,
+    pub childs: Option<Box<[QuadTree; 4]>>,
 }
