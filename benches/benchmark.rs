d530a91cd32c9e750f8096055becabd4887502ec
Titouan Lacombe <titouan.lacombe99@gmail.com>
2023-10-18 09:18:14 +0200

    params

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,5 +1,6 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use nalgebra::Vector2;
+use std::time::Duration;
 
 use iridium::{
     examples::gen_planet,
@@ -26,7 +27,11 @@ fn generate_particles(n: usize) -> Particles {
     particles
 }
 
-fn benchmark_barnes_hut(c: &mut Criterion) {
+fn benchmark_qt(c: &mut Criterion) {
+    let mut group = c.benchmark_group("quadtree");
+    group.warm_up_time(Duration::from_millis(200));
+    group.measurement_time(Duration::from_secs(2));
+
     let particles = generate_particles(1000);
     let mut forces = vec![Vector2::new(0.0, 0.0); particles.len()];
     let mut quadtree = QuadTree::new(
@@ -36,18 +41,32 @@ fn benchmark_barnes_hut(c: &mut Criterion) {
         0.5,
     );
 
-    c.bench_function("insertion", |b| {
+    group.bench_function("insertion", |b| {
+        b.iter(|| {
+            let mut quadtree = QuadTree::new(
+                Rect::new(Vector2::new(0.0, 0.0), Vector2::new(1000.0, 1000.0)),
+                10,
+                Gravity::new(1., 0.),
+                0.5,
+            );
+            quadtree.insert_particles(&particles);
+        })
+    });
+
+    group.bench_function("re-insertion", |b| {
         b.iter(|| {
             quadtree.insert_particles(&particles);
         })
     });
 
-    c.bench_function("barnes_hut", |b| {
+    group.bench_function("barnes_hut", |b| {
         b.iter(|| {
             quadtree.barnes_hut_particles(&particles, &mut forces);
         })
     });
+
+    group.finish();
 }
 
-criterion_group!(benches, benchmark_barnes_hut);
+criterion_group!(benches, benchmark_qt);
 criterion_main!(benches);
