7a0682de5ec99c5cebd0295a17f28d0724f76a68
Titouan Lacombe <titouan.lacombe99@gmail.com>
2023-10-18 13:20:07 +0200

    cdcdcd

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -36,7 +36,7 @@ fn benchmark_qt(c: &mut Criterion) {
     group.warm_up_time(Duration::from_millis(400));
     group.measurement_time(Duration::from_secs(4));
 
-    let particles = generate_particles(1000);
+    let particles = generate_particles(1500);
     let max_particles = 20;
     let theta = 0.5;
     let gravity = Gravity::new(1., 0.);
